1b5e3a9265ead487a1f38120c28029a5276da3ba
mehdibh <mehdibhaddou@gmail.com>
2021-02-20 17:03:02 +0100

    basic : successor & goal-test

--- knight.py
+++ knight.py
@@ -11,24 +11,57 @@ from search import *
 class Knight(Problem):
 
     def successor(self, state):
-        pass
+        moves = [(-2, -1),(-1, -2),(-2, 1),(1, -2),(-1, 2),(2, -1),(1, 2),(2, 1)]
+        old_pos = state.knight_pos
+        states = []
+        for i in range(8):
+            new_pos = (old_pos[0] + moves[i][0],old_pos[1] + moves[i][1])
+            if 0 <= new_pos[0] < state.nRows \
+                and 0 <= new_pos[1] < state.nRows \
+                and new_pos[0] < state.nRows \
+                and new_pos[1] < state.nCols \
+                and state.grid[new_pos[0]][new_pos[1]] == " ":
+
+                potential = self.create_state(old_pos[0],old_pos[1],new_pos[0],new_pos[1],state)
+                states.append(potential)
+
+        return states
 
     def goal_test(self, state):
-        pass
+        goal = (state.nRows * state.nCols) + 1
+        test = 0
+        for i in range(state.nRows):
+            for j in range(state.nCols):
+                tmp = state.grid[i][j]
+                if tmp == u"\u265E":
+                    test += 1
 
+                if tmp == u"\u2658":
+                    test += 2
+
+        return test == goal
+
+    def create_state(self,old_x,old_y,new_x,new_y,state):
+        new_state = State([state.nCols, state.nRows],(new_x, new_y))
+        new_state.grid = [row[:] for row in state.grid]
+        new_state.grid[old_x][old_y] = u"\u265E"
+        new_state.grid[new_x][new_y] = u"\u2658"
+        return new_state
 
 ###############
 # State class #
 ###############
 
 class State:
-    def __init__(self, shape, init_pos):
-        self.nRows = shape[0]
-        self.nCols = shape[1]
+    def __init__(self, shapes, start_pos):
+        self.nRows = shapes[0]
+        self.nCols = shapes[1]
         self.grid = []
         for i in range(self.nRows):
-            self.grid.append([" "]*self.nCols)
-        self.grid[init_pos[0]][init_pos[1]] = "♘"
+            self.grid.append([" "] * self.nCols)
+
+        self.knight_pos = start_pos
+        self.grid[start_pos[0]][start_pos[1]] = "♘"
 
     def __str__(self):
         n_sharp = 2 * self.nCols + 1
@@ -45,6 +78,10 @@ class State:
         s += "#" * n_sharp
         return s
 
+    def tostring(self):
+        for i in self.grid:
+            print(i)
+        print("\n")
 
 ##############################
 # Launch the search in local #
@@ -54,6 +91,18 @@ class State:
 with open('instances.txt') as f:
     instances = f.read().splitlines()
 
+    # Test pour voir si ça calcule les bons successeurs
+    elts = instances[4].split(" ")
+    shape = (int(elts[0]), int(elts[1]))
+    init_pos = (int(elts[2]), int(elts[3]))
+    init_state = State(shape, init_pos)
+
+    init_state.tostring()
+    problem = Knight(init_state)
+    for curr in problem.successor(init_state):
+        curr.tostring()
+
+'''
 for instance in instances:
     elts = instance.split(" ")
     shape = (int(elts[0]), int(elts[1]))
@@ -79,9 +128,9 @@ for instance in instances:
     print("* Path cost to goal:\t", node.depth, "moves")
     print("* #Nodes explored:\t", nb_explored)
     print("* Queue size at goal:\t",  remaining_nodes)
+'''
 
-
-
+'''
 ####################################
 # Launch the search for INGInious  #
 ####################################
@@ -109,3 +158,4 @@ print("* Execution time:\t", str(endTime - startTime))
 print("* Path cost to goal:\t", node.depth, "moves")
 print("* #Nodes explored:\t", nb_explored)
 print("* Queue size at goal:\t",  remaining_nodes)
+'''
